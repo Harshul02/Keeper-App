2a77e543025cfe21faacb44a96ec0080be0d88a7
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-10 23:41:42 +0530

    Added Arrow Function

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Note()
+function Note(props)
 {
     return(
         <div className="note">
-            <h1>Title</h1>
-            <p>Hello How Are you? I will Call you later.</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     )
 }
